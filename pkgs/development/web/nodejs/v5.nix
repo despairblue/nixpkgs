d2fee7d19178301672ff2c2dcbd265c22ed37540
Danny Arnold <despair.blue@gmail.com>
2016-08-09 15:47:51 +0200

    pin node

--- pkgs/development/web/nodejs/v5.nix
+++ pkgs/development/web/nodejs/v5.nix
@@ -4,9 +4,9 @@
 }@args:
 
 import ./nodejs.nix (args // rec {
-  version = "5.12.0";
+  version = "5.9.0";
   src = fetchurl {
     url = "http://nodejs.org/dist/v${version}/node-v${version}.tar.xz";
-    sha256 = "4f926373f11f2a25156eee1804ec012eb912c42e5d34fc2909889da22efdadfe";
+    sha256 = "1frlgs125if91pf37z15bp012vy4i8m0gl7skm2xy8d78j72zpwv";
   };
 })

439698ea2c56ad7c552290abbf8b8d359bf7e115
Danny Arnold <despair.blue@gmail.com>
2016-09-23 12:49:21 +0200

    atom: 1.10.1 -> 1.10.2

--- pkgs/applications/editors/atom/default.nix
+++ pkgs/applications/editors/atom/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "atom-${version}";
-  version = "1.10.1";
+  version = "1.10.2";
 
   src = fetchurl {
     url = "https://github.com/atom/atom/releases/download/v${version}/atom-amd64.deb";
-    sha256 = "0v03a93qa57ajji4sfz7hyr06n20jnlq87103nr7wqycv1v4dm85";
+    sha256 = "1pizmr5i4fnbxykrrqk7g837fj0plazgxrbgyqb9bns5nvwcrk6p";
     name = "${name}.deb";
   };
 
